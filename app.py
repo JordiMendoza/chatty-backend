87c5f4e76d44710dac829b942bf8abae9bb7554e
TuNombreDeUsuario <jordyyymendoza@gmail.com>
2025-06-23 22:21:02 -0600

    Commit AZURE

--- app.py
+++ app.py
@@ -148,4 +148,5 @@ if __name__ == "__main__":
     collection = client.get_collection("harrypotter_chunks")
 
     print("🚀 Iniciando servidor Flask en http://localhost:5000")
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
